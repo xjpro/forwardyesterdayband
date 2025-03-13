ca499c7d87dcf3123237ad0de04afb7196b29271
Joshua Prodahl <jprodahl@gmail.com>
2025-03-13 07:09:34 -0500

    some updates

--- app/page.tsx
+++ app/page.tsx
@@ -5,27 +5,30 @@ export default function Home() {
         <h1>Forward Yesterday</h1>
       </header>
 
+
+      <div className="band-image-bg">
+      <section className="band-image"/>
+      </div>
+
       <section>
         <h2>About Us</h2>
         <p>
           Forward Yesterday is an acoustic trio blending 90s alternative,
-          grunge, and R&B with a fresh, stripped-down sound. Our setlist
-          combines moody, introspective rock with groove-driven energy, offering
-          a dynamic mix of familiar yet reimagined songs. With upright bass,
-          acoustic guitar, and light percussion, we create an intimate
-          atmosphere that brings nostalgia and new life to classic tracks.
+          grunge, and R&B with a fresh, stripped-down sound. Our setlist of moody rock and groove-driven R&B covers offers
+          a mix of familiar yet reimagined songs. With upright bass,
+          acoustic guitar, and light percussion, we bring new life to classic tracks.
         </p>
       </section>
 
       <section>
-        <h2>Where to Find Us</h2>
+        <h2>Where to Find Us Next</h2>
         <p>
           March 23rd 10:30am @ The Finnish Bistro
         </p>
       </section>
 
       <section>
-        <h2>Past Shows</h2>
+        <h2>Past Shows of 2025</h2>
         <p>February 23rd @ The Finnish Bistro</p>
         <p>January 19th @ The Finnish Bistro</p>
       </section>
