5b71e92d33b8e4689c23c4528fdf104606ef5a4c
xjpro <jprodahl@gmail.com>
2025-08-29 09:18:16 -0500

    flex whatrever

--- app/components/SongItem.tsx
+++ app/components/SongItem.tsx
@@ -11,10 +11,22 @@ export default function SongItem({ title, audioSrc }: SongItemProps) {
           display: "flex",
           alignItems: "center",
           gap: "1rem",
-          width: "600px",
+          width: "100%",
+          maxWidth: "600px",
+          flexDirection: "row",
         }}
+        className="song-item-content"
       >
-        <h3 style={{ width: "300px", textAlign: "left", margin: 0 }}>
+        <h3
+          style={{
+            width: "300px",
+            textAlign: "left",
+            margin: 0,
+            minWidth: "0",
+            flex: "0 0 auto",
+          }}
+          className="song-title"
+        >
           {title}
         </h3>
         <audio
